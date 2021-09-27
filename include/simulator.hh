b17d535e5a47979f5d73ed0e230368def20c06a0
Hideharu YAMAUCHI <67892011+HideharuYamauchi@users.noreply.github.com>
2021-09-27 18:17:36 +0900

    Add files via upload

---
+++ include/simulator.hh
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////
+//   High field simulation for MuSEUM Experiment 
+//
+//      Author:  Hideharu Yamauchi 2021/09/18 
+///////////////////////////////////////////////////
+
+#if !defined(___header_simulator_)
+#define ___header_simulator_ 1
+
+#include "TMath.h"
+#include <vector>
+#include "TTree.h"
+#include "TF1.h"
+#include "./HFgeometry.hh"
+#include "../src/RF.cc"
+
+class simulator{
+private:
+  std::vector<double> position;
+  double angle;
+  double solid_angle;
+  int Mode;
+  double state_amp[2];
+  double Btob;
+  double coefficient_s;
+  double coefficient_c;
+  
+public:
+  simulator(int mode, RFfield* GetRFfield){Mode=mode;};
+  simulator(TTree* runtree, int mode);
+  ~simulator(void){;};
+  TF1* f1;
+  void GetXYZ(int x, int y, int z);
+  void timedev(double t, double b, double delta, double gamma);
+  void Vis_state_amp(void);
+  int CalculateBtob(int* position);
+  void CalculateEffectiveb(void);
+};
+#endif
